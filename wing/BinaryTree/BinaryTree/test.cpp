d59fd6e6ccfc036ea9ec0cfbc07db49e57295ee8
liu153 <627736410@qq.com>
2016-05-22 17:22:14 +0800

    测试二叉树镜像

---
+++ wing/BinaryTree/BinaryTree/test.cpp
@@ -0,0 +1,45 @@
+#include"BinaryTree.h"
+
+void Test1()
+{
+	int a1[10] = {1,2,3,'#','#',4,'#','#',5,6};
+	int a2[15] = {1,2,'#',3,'#','#',4,5,'#',6,'#',7,'#','#',8,};
+	int a3[5] = {2,3,'#','#',4};
+
+	BinaryTree<int> t1(a1,10,'#');
+	BinaryTree<int> t2(a1,15,'#');
+	BinaryTree<int> t3(a1,5,'#');
+
+	cout<<"t1遍历二叉树（前序、中序、后序、层次）:"<<endl;
+	t1.PrevOrder();
+	t1.InOrder();
+	t1.PosOrder();
+	t1.LevelOrder();
+}
+
+void Test2()
+{
+	int a1[10] = {1,2,3,'#','#',4,'#','#',5,6};
+	int a2[15] = {1,2,'#',3,'#','#',4,5,'#',6,'#',7,'#','#',8,};
+	int a3[5] = {2,3,'#','#',4};
+
+	BinaryTree<int> t1(a1,10,'#');
+	BinaryTree<int> t2(a1,15,'#');
+	BinaryTree<int> t3(a1,5,'#');
+
+	cout<<t1.Size()<<endl;
+	cout<<t1.Depth()<<endl;
+	cout<<t1.LeafSize()<<endl;
+	t1.MirrorRecursively();
+	t1.PrevOrder();
+	cout<<t1.GetKLevel1(3)<<endl;
+	cout<<t1.GetKLevel2(3)<<endl;
+}
+
+int main()
+{
+	//Test1();
+	Test2();
+	system("pause");
+	return 0;
+}
