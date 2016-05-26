5e86c01dca01285d5b5de8d4b1df09d527cf2792
liu153 <627736410@qq.com>
2016-05-26 17:21:00 +0800

    Test HashTableBucket

--- wing/5-22-HashTable/5-22-HashTable/test.cpp
+++ wing/5-22-HashTable/5-22-HashTable/test.cpp
@@ -54,11 +54,58 @@ void TestDict()
 	dict.Print();
 }
 
+void TestHashTableBucket1()
+{
+	HashTableBucket::HashTableBucket<int,string> ht1;
+	ht1.Insert(51,"¿ª·â");
+	ht1.Insert(105,"¼òÌå");
+	ht1.Insert(52,"²âÊÔ");
+	ht1.Insert(3,"¹şÏ£");
+	ht1.Insert(55,"¿ª·â");
+	ht1.Insert(2,"×Öµä");
+	ht1.Insert(106,"seek");
+	ht1.Insert(53,"liu");
+	ht1.Insert(0,"ipad");
+	ht1.PrintHashTable();
+
+	ht1.Find(2);
+	ht1.Remove(2);
+	ht1.PrintHashTable();
+
+	cout<<endl;
+	HashTableBucket::HashTableBucket<int,string> ht2(ht1);
+	ht2.PrintHashTable();
+}
+
+void TestHashTableBucket2()
+{
+	HashTableBucket::HashTableBucket<int,string> ht1;
+	ht1.Insert(51,"¿ª·â");
+	ht1.Insert(105,"¼òÌå");
+	ht1.Insert(52,"²âÊÔ");
+	ht1.Insert(3,"¹şÏ£");
+	ht1.Insert(55,"¿ª·â");
+	ht1.Insert(2,"×Öµä");
+	ht1.Insert(106,"seek");
+	ht1.Insert(53,"liu");
+	ht1.Insert(0,"ipad");
+	ht1.PrintHashTable();
+	cout<<endl;
+
+	HashTableBucket::HashTableBucket<int,string> ht2(ht1);
+	ht2.PrintHashTable();
+	cout<<endl;
+
+	HashTableBucket::HashTableBucket<int,string> ht3 = ht1;
+	ht3.PrintHashTable();
+}
 int main()
 {
 	//TestHash1();
 	//TestHash2();
-	TestDict();
+	//TestDict();
+	//TestHashTableBucket1();
+	TestHashTableBucket2();
 	system("pause");
 	return 0;
 }
