5bb498ddd99b981f1e9b1a0a90a36a073e32bcc8
liu153 <627736410@qq.com>
2016-05-28 21:21:47 +0800

    Test Array Spin

--- offer.test8/offer.test8/test.cpp
+++ offer.test8/offer.test8/test.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
-	int arr[] = {3,4,5,1,2};
-	cout<<SpinArrayMin(arr,5)<<endl;
+	int arr1[] = {3,4,5,1,2};
+	int arr2[] = {1,1,1,0,1};
+	cout<<SpinArrayMin(arr1,5)<<endl;
+	cout<<Min(arr2,5)<<endl;
 	system("pause");
 	return 0;
 }
