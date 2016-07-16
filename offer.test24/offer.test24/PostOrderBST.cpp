4ba2a1156f7bc650b461d233d790350da3af9bea
liu153 <627736410@qq.com>
2016-07-16 10:01:28 +0800

    判断一个序列是否为二叉搜索树的前序、后序遍历序列

--- offer.test24/offer.test24/PostOrderBST.cpp
+++ offer.test24/offer.test24/PostOrderBST.cpp
@@ -32,14 +32,47 @@ bool verifySequenceOfBST(int sequence[],int length)
 	{
 		right = verifySequenceOfBST(sequence + i,length - i - 1);
 	}
-
 	return left && right;
+}
+
+//判断一个序列是否为二叉搜索树的后序遍历序列
+bool PrevSequenceOfBST(int sequence[] ,int length)
+{
+	if(sequence == NULL || length <= 0)
+		return false;
+	int root = sequence[0];//第一个节点为根节点
+	int i = 1;
+	for(;i < length;i++)//比root小的都为左子树
+	{
+		if(sequence[i] > root)
+			break;
+	}
+	int j = i;
+	for(;j < length;j++)//比root大的是右子树
+	{
+		if(sequence[j] < root)
+			return false;
+	}
+
+	bool left = true;//递归遍历左子树
+	if(i > 1)
+	{
+		left = PrevSequenceOfBST(sequence + 1,i);
+	}
+	bool right = true;//递归遍历右子树
+	if(j < length)
+	{
+		right = PrevSequenceOfBST(sequence + i,length - i);
+	}
+	return (left && right);
 }
 
 int main()
 {
 	int a[] = {5,7,6,9,11,10,8};
+	int b[] = {8,6,5,7,10,9,11};
 	cout<<"序列a是否为搜索二叉树的后序遍历序列？"<<verifySequenceOfBST(a,sizeof(a)/sizeof(a[0]))<<endl;
+	cout<<"序列b是否为搜索二叉树的前序遍历序列？"<<PrevSequenceOfBST(b,sizeof(b)/sizeof(b[0]))<<endl;
 	system("pause");
 	return 0;
 }
